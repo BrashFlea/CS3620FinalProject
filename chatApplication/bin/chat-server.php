b528fa3ff580654aeb5d7037731fce1f08ef8871
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2016-12-13 14:15:50 -0700

    Bugfixed socket connection. Application works when connected to WSU network

--- chatApplication/bin/chat-server.php
+++ chatApplication/bin/chat-server.php
@@ -13,17 +13,16 @@ use MyApp\Chat;
 
     require dirname(__DIR__) . '/vendor/autoload.php';
     //Web connect (Doesn't work on icarus)
-    /*$server = IoServer::factory(
+    $server = IoServer::factory(
         new HttpServer(
             new WsServer(
                 new Chat()
             )
         ),
-        8081, //8080 was taken
-        '127.0.0.1'
+        8081 //8080 was taken
 
-    );*/
+    );
 
     //Working telnet connection
-    $server = IoServer::factory(new Chat, 8081, '127.0.0.1');
+    //$server = IoServer::factory(new Chat, 8081, '127.0.0.1');
     $server->run();
